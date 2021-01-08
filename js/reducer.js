12b5004b98437474ff110a59b4cd0f14b8c6bb0f
Riley Slayden <riley@Rileys-MacBook-Pro.local>
2021-01-08 11:22:11 -0700

    commit tree 9c872c31eb85745fa420afea8a781e096e1af05f

--- js/reducer.js
+++ js/reducer.js
@@ -1,13 +1,15 @@
+let state = {count: 0};
+
 function changeState(state, action){
-  switch (action.type) {
-    case 'INCREASE_COUNT':
-      return {count: state.count + 1}
-    default:
-      return state;
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
   }
+
+function dispatch(action){
+  state = changeState(state, action)
+  return state
 }
-
-let state = {count: 0}
-let action = {type: 'INCREASE_COUNT'}
-
-changeState(state, action)
